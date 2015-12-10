3522dc72f83a47a4733678b2d0fc1b85519f8365
wangyix <wangyix@gmail.com>
2015-12-09 20:30:57 -0800

    exposed computeHankels() and computeLengendrePolys()

--- of/apps/myApps/Particles/src/utils.h
+++ of/apps/myApps/Particles/src/utils.h
@@ -10,7 +10,9 @@ void signedMoment2(const ofMeshFace& tri, float* Mxx, float* Myy, float* Mzz,
                                           float* Mxy, float* Myz, float* Mxz);
 
 void computeYConstants(int N, vector<double>& C_storage, vector<double*>& C);
+void computeHankels(double kr, int N, vector<complex<double>>& h);
 void computeHankelsAndDerivatives(double kr, int N, vector<complex<double>>& h, vector<complex<double>>& h1);
+void computeLegendrePolys(double x, int N, vector<double>& P_storage, vector<double*>& P);
 void computeLegendrePolysAndDerivatives(double x, int N, vector<double>& P_storage, vector<double*>& P,
                                         vector<double>& P1_storage, vector<double*>& P1);
 
